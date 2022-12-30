dbdbd336fad84b364926572955ec294a354b6176
shenhan <shenhanfly@163.com>
2022-12-30 21:30:59 +0800

    xiu

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <router-link to="/about">hello</router-link>
+    <router-link to="/about">word</router-link>
+
+    <router-link to="/about">222</router-link>
   </div>
   <router-view/>
 </template>
